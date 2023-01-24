c991355855d81006bea5c30c08134d0201ea1f9c
ZzIsGod1019 <1498852723@qq.com>
2023-01-24 19:56:53 +0800

    update

--- src/ring.rs
+++ src/ring.rs
@@ -1,40 +1,30 @@
-use std::{collections::{BTreeMap, hash_map::DefaultHasher}, fmt::Error, hash::{Hash, Hasher}};
+use std::{collections::{BTreeMap, hash_map::DefaultHasher, HashMap}, hash::{Hash, Hasher}};
 
 use anyhow::{anyhow, Result};
 
-pub struct Ring<'a, T: Eq> {
-    link: BTreeMap<i32, Vec<&'a RingItem<T>>>,
-}
-
-pub struct RingItem<T: Eq> {
-    inner: T,
-    tag: String,
-}
-
-impl<T: Eq> RingItem<T> {
-    pub fn new(t: T, tag: String) -> Self {
-        RingItem { inner: t , tag}
-    }
-
-    pub fn get_tag(&self) -> String {
-        String::from(&self.tag)
-    }
+pub struct Ring<T> {
+    link: BTreeMap<i32, Vec<String>>,
+    map: HashMap<String, T>
 }
 
 const MAX_INT : i32 = 65535;
 const PER_COUNT : usize = 100;
 
-impl<'a, T: Eq> Ring<'a, T> {
+impl<T> Ring<T> {
     // 添加项目
-    pub fn add_item(&mut self, item: &'a RingItem<T>) -> Result<(), Error> {
-        let tag = item.get_tag();
+    pub fn add_item(&mut self, item: T, tag: String) -> Result<()> {
+        if self.map.contains_key(&tag) {
+            return Err(anyhow!("tag exists"));
+        }
+        self.map.insert(tag.clone(), item);
+
         for i in 0..PER_COUNT {
-            let key = self.hash_index( tag.clone()+ &(i.to_string()));
+            let key = self.hash_index( tag.clone() + &(i.to_string()));
 
             match self.link.get_mut(&key) {
-                Some(items) => items.push(item),
+                Some(items) => items.push(tag.clone()),
                 None => {
-                    self.link.insert(key, vec![item]);
+                    self.link.insert(key, vec![tag.clone()]);
                 },
             }
         }
@@ -42,13 +32,14 @@ impl<'a, T: Eq> Ring<'a, T> {
     }
 
     // 删除项目
-    pub fn remove_item(&mut self, item: &'a RingItem<T>) -> Result<(), Error> {
+    pub fn remove_item(&mut self, tag: String) -> Result<()> {
+        // let item = self.map.get(&tag).unwrap();
         for i in 0..PER_COUNT {
-            let key = self.hash_index(item.get_tag() + &(i.to_string()));
+            let key = self.hash_index(tag.clone() + &(i.to_string()));
 
             match self.link.get_mut(&key) {
                 Some(items) => {
-                    items.retain(|x| x.inner != item.inner);
+                    items.retain(|x| x != &tag);
                 },
                 None => {},
             }
@@ -57,29 +48,35 @@ impl<'a, T: Eq> Ring<'a, T> {
     }
 
     // 选择节点
-    pub fn select(&mut self, index: i32) -> Result<&T> {
-        let mut res = Err(anyhow!("lock poisoned"));
+    pub fn select(&mut self, index: i32) -> Option<&T> {
+        let mut tag = None;
         for items in self.link.iter_mut() {
-            if res.is_err() || *items.0 > index {
+            if tag.is_none() || *items.0 > index {
                 let len = items.1.len() as i32;
-                res = Ok(&items.1[(index % len) as usize].inner)
+                tag = Some(items.1[(index % len) as usize].clone());
             }
         }
-        res
+        if tag.is_some() {
+            return self.map.get(&(tag.unwrap()));
+        }
+        None
     }
 
     // 加密后的偏移量
-    fn hash_index(&mut self, content: String) -> i32 {
+    fn hash_index(&mut self, tag: String) -> i32 {
         let mut s = DefaultHasher::new();
-        content.hash(&mut s);
+        tag.hash(&mut s);
         let hash = s.finish();
         (hash % (MAX_INT as u64)) as i32
     }
 }
 
-impl<'a, T: Eq> Default for Ring<'a, T> {
+impl<T> Default for Ring<T> {
     fn default() -> Self {
-        Self { link: BTreeMap::new() }
+        Self {
+            link: BTreeMap::new(),
+            map: HashMap::new()
+        }
     }
 }
 
@@ -96,8 +93,7 @@ mod tests {
     #[test]
     fn add_item_test() {
         let mut ring = Ring::default();
-        let item1 = RingItem::new(1, String::from("tag:1"));
-        ring.add_item(&item1).unwrap();
+        ring.add_item(1, String::from("tag:1")).unwrap();
         let mut node_count = 0;
         for items in ring.link.values() {
             node_count += items.len();
@@ -108,11 +104,9 @@ mod tests {
     #[test]
     fn remove_item_test() {
         let mut ring = Ring::default();
-        let item1 = RingItem::new(1, String::from("tag:1"));
-        let item2 = RingItem::new(2, String::from("tag:2"));
-        ring.add_item(&item1).unwrap();
-        ring.add_item(&item2).unwrap();
-        ring.remove_item(&item2).unwrap();
+        ring.add_item(1, String::from("tag:1")).unwrap();
+        ring.add_item(2, String::from("tag:2")).unwrap();
+        ring.remove_item(String::from("tag:2")).unwrap();
 
         let mut node_count = 0;
         for item in ring.link.values() {
@@ -122,8 +116,8 @@ mod tests {
 
         for items in ring.link {
             for item in items.1 {
-                if item.inner == item2.inner {
-                    assert_eq!(true, false)
+                if item == String::from("tag:2") {
+                    assert_eq!(true, false);
                 }
             }
         }
@@ -132,10 +126,8 @@ mod tests {
     #[test]
     fn select_test() {
         let mut ring = Ring::default();
-        let item1 = RingItem::new(1, String::from("tag:1"));
-        let item2 = RingItem::new(2, String::from("tag:2"));
-        ring.add_item(&item1).unwrap();
-        ring.add_item(&item2).unwrap();
+        ring.add_item(1, String::from("tag:1")).unwrap();
+        ring.add_item(2, String::from("tag:2")).unwrap();
 
         let hash1 = 4567;
         let t1 = *ring.select(hash1).unwrap();
